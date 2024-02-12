7469c18ec239435fb23c3985ab986682c5c14f49
VictorTazoi <victortazoi@hotmail.com>
2024-02-11 23:43:18 -0300

    Menu pronto

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ const botaoTema = document.getElementById('botaoTema'); //Botao de alterar o tem
 
 botaoTema.addEventListener('click', alterarTema); //Adiciona ação ao botao de alterar o tema
 
+var statusTema = 'dark';
 
 // FUNÇÃO PARA ALTERAR O TEMA
 function alterarTema(){
-    const temaBody = document.getElementById('body');
+    const conteudo = document.getElementById('conteudo');
     const linhaLateral = document.getElementById('linhaLateral');
     const colunaMenu = document.getElementById('colunaMenu');
     const nome = document.getElementById('nome');
@@ -15,10 +16,10 @@ function alterarTema(){
     const colunaConteudo = document.getElementById('colunaConteudo');
     const botaoTema = document.getElementById('botaoTema');
 
-    if(temaBody.classList.contains('fundo-dark')){
-        //Body
-        temaBody.classList.remove('fundo-dark');
-        temaBody.classList.add('fundo-light');
+    if(statusTema == 'dark'){
+        //Conteudo
+        conteudo.classList.remove('fundo-dark');
+        conteudo.classList.add('fundo-light');
         //Linha lateral
         linhaLateral.classList.remove('fundo-light');
         linhaLateral.classList.add('fundo-dark');
@@ -43,10 +44,12 @@ function alterarTema(){
         //Botao tema
         botaoTema.classList.remove('btn-dark');
         botaoTema.classList.add('btn-light');
-    }else if(temaBody.classList.contains('fundo-light')){
-        //Body
-        temaBody.classList.remove('fundo-light');
-        temaBody.classList.add('fundo-dark');
+
+        statusTema = 'light';
+    }else if(statusTema == 'light'){
+        //Conteudo
+        conteudo.classList.remove('fundo-light');
+        conteudo.classList.add('fundo-dark');
         //Linha lateral
         linhaLateral.classList.remove('fundo-dark');
         linhaLateral.classList.add('fundo-light');
@@ -72,6 +75,8 @@ function alterarTema(){
         //Botao tema
         botaoTema.classList.remove('btn-light');
         botaoTema.classList.add('btn-dark');
+
+        statusTema = 'dark';
     }else{
         window.alert('Erro ao alterar o tema! Favor, entrar em contato com o desenvolvedor!')
     }
